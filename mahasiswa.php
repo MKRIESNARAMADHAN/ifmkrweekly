297fe4be3e67c6bc476c2e21ac73dc8ca10ea58d
MKRIESNARAMADHAN <inichanel321@gmail.com>
2026-06-24 10:22:12 +0700

    mysql

--- mahasiswa.php
+++ mahasiswa.php
@@ -1,6 +1,6 @@
 <?php
 
-    $koneksi = mysqli_connect("localhost", "root", "", "ifmkrweekly");
+    $koneksi = mysqli_connect("localhost", "root", "root", "ifmkrweekly");
 
     if($koneksi)
     {
